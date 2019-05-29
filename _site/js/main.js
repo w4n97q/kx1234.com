30b1f118f5ff6ab5c0f23974576d88ffc12ce289
wjq <disdot@foxmail.com>
2019-05-29 23:28:26 +0800

    udpate

--- _site/js/main.js
+++ _site/js/main.js
@@ -59,35 +59,6 @@
 
 			// Parallax background.
 
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
-
-				if (settings.parallax) {
-
-					skel.on('change', function() {
-
-						if (skel.breakpoint('medium').active) {
-
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
-
-						}
-						else {
-
-							$header.css('background-position', 'left 0px');
-
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
-
-						}
-
-					});
-
-				}
-
 		// Main Sections: Two.
 
 			// Lightbox gallery.
